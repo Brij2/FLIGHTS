8f4dc8259ab292c00d098e2733847ca1fc79fd8e
Brij2 <brij4268@gmail.com>
2022-03-06 13:16:00 +0530

    time ass

--- Main2.java
+++ Main2.java
@@ -3,12 +3,15 @@ public class Main2
 {
      public static void main( String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter integer 1: ");
+        System.out.println("enter timr in seconds: ");
         int a = sc.nextInt();
-        System.out.println("enter integer 2");
-        int b = sc.nextInt();
-        System.out.println("sum = " + (a+b));
-
-        
+        // System.out.println("enter float no. 2");
+         float b =a/3600;
+         a = a%3600;
+        System.out.println("time :: \nhour: " + b);
+        b =a/60;
+        a = a%60;
+        System.out.println("minutes: " + b);
+        System.out.println("seconds: " + a);
     }
 }
