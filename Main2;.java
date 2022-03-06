124203772d5bffc486207475305deed4bafd6f7f
Brij2 <brij4268@gmail.com>
2022-03-06 11:35:12 +0530

    ass2

---
+++ Main2;.java
@@ -0,0 +1,13 @@
+import java.util.scanner;
+public Class Main2()
+{
+     public static void main( String[] args) {
+        scanner sc = new scanner(System.in);
+        System.out.println("enter integer 1: ");
+        int a = sc.nextint();
+        int b = sc.nextint();
+        System.out.println("sum = " + a+b);
+
+        
+    }
+}
