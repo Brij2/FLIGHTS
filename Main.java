c871c2de62c455d9c4dc6ea425eac3b673ec25e1
Brij2 <brij4268@gmail.com>
2022-03-06 11:09:43 +0530

    c1

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter name : ");
+    String a = sc.next();
+    System.out.println("name is: " + a);
+
+
   }
 }
-//hjhj
+
