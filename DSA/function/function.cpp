54d2f0b907e5123776c047334d83b94eca455d5f
Brij2 <brij4268@gmail.com>
2022-03-14 19:44:42 +0530

    m

--- DSA/function/function.cpp
+++ DSA/function/function.cpp
@@ -19,7 +19,12 @@ int main()
     int n; 
     cin>>n;
     for(int i=0; i<n; i++)
-        for(int j=0; j<=i; j++)
-            nco(i,j);
+     {  for(int j=0; j<=i; j++)
+          {
+             nco(i,j);
+           
+          } 
+           cout<<"\n";
+    }
     return 0; 
 }
